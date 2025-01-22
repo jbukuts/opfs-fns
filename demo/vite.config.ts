69fdd6949d9ac87f9afc336a97194ce1c9df4d66
jbukuts <jbukuts@gmail.com>
2025-01-22 14:34:32 -0500

    fix: add base string to conf

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/opfs-fns/',
   resolve: {
     alias: {
       '#': path.resolve(__dirname, './src')
